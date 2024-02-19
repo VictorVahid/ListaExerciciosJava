6ec55aab86a8736975b7625f4c47cf943fc3f7fd
Victor Vahid <victorvahid76@example.com>
2024-02-19 16:27:31 -0400

    Exercicio 3

--- src/ExercicioDois.java
+++ src/ExercicioDois.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class ExercicioDois {
+import java.util.Scanner;
+
+public class ExercicioDois {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//variaveis
+		float raioCirculo, areaCirculo, pi;
+		pi = 3.14159F;
+
+		//Entrada de Dados
+		System.out.println("Digite o raio do circulo: ");
+		raioCirculo = sc.nextFloat();
+
+
+		//Calculo
+		areaCirculo = (float) (pi * Math.pow(raioCirculo, 2));
+
+		//Output
+		System.out.printf("A = %.4f",areaCirculo);
+
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		//Exercicio 1
+		int numeroUm = 0, numeroDois = 0;
+		System.out.println("Digite um número: ");
+		numeroUm = scanner.nextInt();
+		System.out.println("Digite o segundo número: ");
+		numeroDois = scanner.nextInt();
+
+		int soma = numeroUm + numeroDois;
+
+		System.out.printf("A soma é : %d", soma);
+
+		//Exercicio 2
+
+
+
+		scanner.close();
+	}
 }
