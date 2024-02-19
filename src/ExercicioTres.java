eaea433bd1835545c92bcb0535ff0e008df8ac58
Victor Vahid <victorvahid76@example.com>
2024-02-19 16:25:54 -0400

    Exercicio 3

---
+++ src/ExercicioTres.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ExercicioTres {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//Variaveis
+		int NumeroA, NumeroB, NumeroC, NumeroD;
+		int Resultado;
+
+		//Entrada
+		System.out.println("Digite o primeiro número: ");
+		NumeroA = sc.nextInt();
+		System.out.println("Digite o segundo número: ");
+		NumeroB = sc.nextInt();
+		System.out.println("Digite o terceiro número: ");
+		NumeroC = sc.nextInt();
+		System.out.println("Digite o quarto número: ");
+		NumeroD = sc.nextInt();
+		//Calculos
+
+		Resultado = (NumeroA * NumeroB) - (NumeroC * NumeroD);
+
+		//Saida
+		System.out.printf("A diferença é : %d", Resultado);
+
+		sc.close();
+
+
+	}
+}
