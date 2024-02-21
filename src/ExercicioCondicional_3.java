b0b73ee548700bdd12a8f56f54db77f90abf1b9c
Victor Vahid <victorvahid76@example.com>
2024-02-21 02:40:37 -0400

    Finalização Exercicio Condicional Tres! ✅

--- src/ExercicioCondicional_3.java
+++ src/ExercicioCondicional_3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ExercicioCondicionalTres {
+public class ExercicioCondicional_3 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
