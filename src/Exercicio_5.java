d0a0d834aa5ca040a7f308a012b7895ead4827d2
Victor Vahid <victorvahid76@example.com>
2024-02-21 02:39:08 -0400

    Finalização ExercicioCinco! ✅

--- src/Exercicio_5.java
+++ src/Exercicio_5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ExercicioCinco {
+public class Exercicio_5 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		//Variaveis
