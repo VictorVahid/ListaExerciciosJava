6052ce1582219733b4ba554ab61a0792f9c594c6
Victor Vahid <victorvahid76@example.com>
2024-02-21 02:38:51 -0400

    Finalização ExercicioQuatro! ✅

--- src/Exercicio_4.java
+++ src/Exercicio_4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ExercicioQuatro {
+public class Exercicio_4 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		//Variaveis
