a4568d02c8c774574707e4fd6f7d66157fdb0826
Victor Vahid <victorvahid76@example.com>
2024-03-04 15:22:25 -0400

    Finalização Estrutura For 02! ✅

---
+++ src/ExercicioEstruturaFor_02.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ExercicioEstruturaFor_02 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		int in = 0;
+		int out = 0;
+
+		for (int i = 0; i < N; i++) {
+			int x = sc.nextInt();
+			if (x >= 10 && x <= 20) {
+				in += 1;
+			} else {
+				out += 1;
+			}
+		}
+		System.out.println("In " + in);
+		System.out.println("Out " + out);
+		sc.close();
+	}
+}
