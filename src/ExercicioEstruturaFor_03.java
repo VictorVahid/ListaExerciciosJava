c9acf3762f4aa7aa42beebfa316a223ca798aa7d
Victor Vahid <victorvahid76@example.com>
2024-03-04 15:22:35 -0400

    Finalização Estrutura For 03! ✅

---
+++ src/ExercicioEstruturaFor_03.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ExercicioEstruturaFor_03 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		for (int i = 0; i <= N; i++) {
+			double a = sc.nextDouble();
+			double b = sc.nextDouble();
+			double c = sc.nextDouble();
+
+			double media = (a * 2.0 + b * 3.0 + c * 5.0) / 10.0;
+			System.out.printf("%.1f%n", media);
+		}
+
+		sc.close();
+	}
+}
